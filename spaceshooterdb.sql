64b99b3fcd54005ca539ec251bbe690ad4022765
Sam <38429454+SamFredrickson@users.noreply.github.com>
2020-01-31 20:08:31 +0300

    Add files via upload

---
+++ spaceshooterdb.sql
@@ -0,0 +1,20 @@
+create table Player(
+	[id] int primary key identity not null,
+	[name] nvarchar(max) not null,
+);
+
+create table Score(
+
+	[id] int primary key identity not null,
+	[seconds] int not null,
+	[minutes] int,
+	[hours]   int,
+	[difficulty] nvarchar(max) not null
+);
+create table PlayerScore(
+
+	[id] int primary key identity not null,
+	[id_player] int foreign key references Player([id]),
+	[id_score] int unique foreign key references Score([id]),
+
+);
